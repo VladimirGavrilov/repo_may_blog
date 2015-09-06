4a5701f9aae0ca6bc0e001c4ede81c9aa7155541
VladimirGavrilov <gavriloff.v@yandex.ru>
2015-09-06 22:47:35 +0300

    подключение боотстрапа

---
+++ views/article.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+ <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href=""/>
+    <script type="text/javascript"  src=""></script>
+     
+  <title>Мой  блог  </title>
+  </head>
+ <body>
+   <div class="container">
+   	<h1>Мой блог</h1>
+   	<div>
+	   	<h3><? =$article['content']?></h3>
+	   	<em>Опубликовано: <?php =$article['date'] ?></em>
+   		<p><?php $article['content']?></p>
+   	</div>
+   	<footer><p>Мой блог<dr>Copyright &copy; 2015</p></footer>
+   </div>
+ </body>
+</html>
