ce0c7a5572d0dc719ece5cb673b1595745d3eea2
VladimirGavrilov <gavriloff.v@yandex.ru>
2015-09-07 00:15:30 +0300

    превью

--- views/articles.php
+++ views/articles.php
@@ -1,24 +1,26 @@
 <!DOCTYPE html>
+<html>
  <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href=""/>
+     
     
     <script type="text/javascript"  src=""></script>
      
-  <title>Мой  блог  </title>
+  <title>Мой  блог</title>
   </head>
  <body>
    <div class="container">
    	<h1>Мой блог</h1>
-   	<?php foreach($article as $a  ):?>
+   	 <?php  foreach($articles as $a): ?>
    	<div class="article">   		
-	   	<h3><a href="article.php?id= <?php $a['id']= ?>"><? =$a['title']?></a></h3>
-	   	<em>Опубликовано: <?php =$a['date'] ?></em>
-   		<p><?php $a['content']?></p>
+	   	<h3><?=$a['title']?></h3>
+	   	<em>Опубликовано: <?=$a['date']?></em>
+   		<p><?=$a['content']?></p>
    	</div>
-   	<?php endforeach ?>
-   	<footer><p>Мой блог<dr>Copyright &copy; 2015</p></footer>
+   	<?php endforeach ?> 
+   	<footer><p>Мой блог <br>Copyright &copy; 2015</p></footer>
    </div>
  </body>
 </html>
+ 
