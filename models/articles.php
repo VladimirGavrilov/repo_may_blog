e7e09d1ba804cff473aba143a4c9b50c586db38c
VladimirGavrilov <gavriloff.v@yandex.ru>
2015-09-08 01:29:46 +0300

    подключение к бд

--- models/articles.php
+++ models/articles.php
@@ -1,16 +1,25 @@
 <?php
 
-	function articles_all(){
-		$arc1 = ["id"=>1, "title"=>"Title1", "date"=>"2015-01-01", "content"=>"content1"];
-		$arc2 = ["id"=>2, "title"=>"Title2", "date"=>"2015-02-01", "content"=>"content2"];
+	function articles_all($link){
+		 
+		//Запрос к базе
+		$query = "SELECT * FROM articles ORDER BY id DESC";		 
+		$result = mysqli_query($link, $query);
+ 		 
+		if(!$result) die(mysqli_error($link));
+		//извление из бд
+		$n = mysqli_num_rows($result);
 		
-		$arr[0] = $arc1;
-		$arr[1] = $arc2;
-
-		return $arr;
+		$articles = array();
+		
+		for($i = 0; $i < $n; $i++) {
+			$row = mysqli_fetch_assoc($result);			
+			$articles[] = $row;	
+		}
+		 return $articles;
 	}
-	function articles_get($id){
-		return ["id"=>$id, "title"=>"Просто заголовок", "date"=>"25-01-01", "content"=>"Здесь статья"];
+	function articles_get($link, $id_article){
+ 
 	}
 	function articles_edit($id, $title, $date, $content){
 		
