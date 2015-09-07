e37bd7cfa16e55ba00e63ed5119d43a56d0ec7e1
VladimirGavrilov <gavriloff.v@yandex.ru>
2015-09-07 18:54:52 +0300

    коментарии

--- article.php
+++ article.php
@@ -1,10 +1,9 @@
 <?php
-	require_once("datebase.php");
+	require_once("database.php");
 	require_once("models/articles.php");
-	
+	echo $_GET['id'];	
 	$articles = articles_get($_GET['id']);
- 
+	//подключаем вид	
+	require_once("views/article_view.php");
 	
-	require_once("views/article.php");
-	 
 ?>
