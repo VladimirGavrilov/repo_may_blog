bc0906ecdefd993164d89613501fb2a01180c00c
VladimirGavrilov <gavriloff.v@yandex.ru>
2015-09-08 01:30:49 +0300

    подключение к бд

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 	require_once("database.php");
 	require_once("models/articles.php");
-	
-	$articles = articles_all();
-	
+	$link = db_connect();
+	$articles = articles_all($link);
+	 
 	require_once("views/articles_view.php");
     
 	 
