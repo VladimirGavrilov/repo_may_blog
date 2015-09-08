18ecb18ba0ebe779f27ec064b24fe043ed11d2f1
VladimirGavrilov <gavriloff.v@yandex.ru>
2015-09-08 18:49:32 +0300

    article_get db conect

--- models/articles.php
+++ models/articles.php
@@ -13,13 +13,36 @@
 		$articles = array();
 		
 		for($i = 0; $i < $n; $i++) {
-			$row = mysqli_fetch_assoc($result);			
-			$articles[] = $row;	
+			$row = mysqli_fetch_assoc($result);
+			
+			$articles[] = $row;
+		
 		}
 		 return $articles;
 	}
 	function articles_get($link, $id_article){
- 
+		//Запрос к базе
+		$query = "SELECT * FROM articles WHERE  id = $id_article ";		 
+		$result = mysqli_query($link, $query);
+ 		 
+		if(!$result) die(mysqli_error($link));
+		//извление из бд
+		$n = mysqli_num_rows($result);
+		
+		$article = array();
+		
+		for($i = 0; $i < $n; $i++) {
+			$row = mysqli_fetch_assoc($result);
+			
+			$article = $row;
+		
+		}
+		 return $article;
+		
+		
+		
+		
+		
 	}
 	function articles_edit($id, $title, $date, $content){
 		
