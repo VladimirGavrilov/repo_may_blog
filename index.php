505dcaa361a1ef37c21d75d702c87cfc56e7037a
VladimirGavrilov <gavriloff.v@yandex.ru>
2015-09-08 18:48:50 +0300

    db conect

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 	require_once("database.php");
 	require_once("models/articles.php");
-	$link = db_connect();
-	$articles = articles_all($link);
-	 
+		$link = db_connect();
+		$articles = articles_all($link);
+	
 	require_once("views/articles_view.php");
     
 	 
