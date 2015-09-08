64e1c5f3b290c7b916a205e775c18b72f6cea72c
VladimirGavrilov <gavriloff.v@yandex.ru>
2015-09-08 18:50:10 +0300

    gat_article db

--- article.php
+++ article.php
@@ -1,8 +1,10 @@
 <?php
 	require_once("database.php");
 	require_once("models/articles.php");
-	echo $_GET['id'];	
-	$articles = articles_get($_GET['id']);
+	$link = db_connect();
+	
+	$article = articles_get($link, $_GET['id']);
+	
 	//подключаем вид	
 	require_once("views/article_view.php");
 	
