0b5244562eac71f2778a4fde2c968f9136eea9df
VladimirGavrilov <gavriloff.v@yandex.ru>
2015-09-09 22:48:12 +0300

    вывод картинок в показе всех статей

--- views/articles_view.php
+++ views/articles_view.php
@@ -19,9 +19,15 @@
    	<div class="article">   		
 	   	<h3><a href="article.php?id=<?=$a['id']?>"><?=$a['title']?></a></h3>	   	
 	   	<em>Опубликовано: <?=$a['date']?></em>	  
-   		<p><?=$a['content']?></p>
+   		<p>   		
+	   		<?=$a['content']?>
+	   		<?php  foreach($a['src'] as $img): ?>					
+					 <img class="illustrazia" src="<?=$img['src_image']?>" alt="<?=$img['alt_image']?>"/>				 
+			  <?php endforeach ?>
+   		</p>
    	</div>
    	<?php endforeach ?> 
+   	 
    	<footer><p>Мой блог <br><a href="index.php">Все статьи</a><br>Copyright &copy; 2015</p></footer>
    </div>
  </body>
