b914c2000fbcd2d99d1d72a37d9ef4c731bec8dc
VladimirGavrilov <gavriloff.v@yandex.ru>
2015-09-09 21:54:07 +0300

    подлючаеь картинки

--- index.php
+++ index.php
@@ -3,7 +3,9 @@
 	require_once("models/articles.php");
 		$link = db_connect();
 		$articles = articles_all($link);
-	
+		 
+	    
+	  
 	require_once("views/articles_view.php");
     
 	 
--- models/articles.php
+++ models/articles.php
@@ -3,8 +3,10 @@
 	function articles_all($link){
 		 
 		//Запрос к базе
-		$query = "SELECT * FROM articles ORDER BY id DESC";		 
+		$query = "SELECT * FROM articles ORDER BY id DESC";	
+			 
 		$result = mysqli_query($link, $query);
+		
  		 
 		if(!$result) die(mysqli_error($link));
 		//извление из бд
@@ -18,6 +20,15 @@
 			$articles[] = $row;
 		
 		}
+		$images = array();
+		 foreach($articles as $i){
+			$src = image_get($link, $i['id']);
+			$images[] = $src;
+				 
+		 };
+		 $articles[] = $images;
+		 
+		var_dump($articles[3]);
 		 return $articles;
 	}
 	function articles_get($link, $id_article){
@@ -35,7 +46,7 @@
 			$row = mysqli_fetch_assoc($result);
 			
 			$article = $row;
-		
+			
 		}
 		 return $article;
 		
@@ -53,5 +64,27 @@
 	function articles_del($id) {
 		
 	}
+	function image_get($link, $id_article){
+	//Запрос к базе
+		$query = "SELECT * FROM images WHERE  id = $id_article ";	
+			 
+		$result = mysqli_query($link, $query);
+		
+ 		 
+		if(!$result) die(mysqli_error($link));
+		//извление из бд
+		$n = mysqli_num_rows($result);
+		
+		$images = array();
+		
+		for($i = 0; $i < $n; $i++) {
+			$row = mysqli_fetch_assoc($result);
+			
+			$images[] = $row;
+		
+		}
+		 return $images;
+	}
+		
 	
 ?>
